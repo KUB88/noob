23709b19ff1dbdf8fa5a42ec5aff8b3211cc440b
KUB88 <718746159@qq.com>
2016-07-16 03:32:23 +0800

    basic model
    basic model

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+class Articles(models.Model):
+	title = models.CharField(max_length = 150)
+	article = models.TextField()
+	timestamp = models.DateTimeField()
+	tag = models.CharField(max_length = 150)
+
+class ArticlesAdmin(admin.ModelAdmin):
+	list_display = ('title', 'tag', 'timestamp')
+
+admin.site.register(Articles)
